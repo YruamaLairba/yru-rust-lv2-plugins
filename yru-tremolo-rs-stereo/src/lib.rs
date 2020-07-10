6359a16a97b683580d58b3a16ebf4393b31c1775
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-07-10 20:41:28 +0200

    make the tremolo stereo

--- yru-tremolo-rs-stereo/src/lib.rs
+++ yru-tremolo-rs-stereo/src/lib.rs
@@ -13,15 +13,18 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 use lv2_core::prelude::*;
-use urid::*;
 use std::f32::consts::PI;
+use urid::*;
 
 #[derive(PortCollection)]
 struct Ports {
     depth: InputPort<Control>,
     rate: InputPort<Control>,
-    input: InputPort<Audio>,
-    output: OutputPort<Audio>,
+    phase: InputPort<Control>,
+    l_in: InputPort<Audio>,
+    r_in: InputPort<Audio>,
+    l_out: OutputPort<Audio>,
+    r_out: OutputPort<Audio>,
 }
 
 /// A plugin to demonstrate how to make preset. This is fully handled by rdf spec, so the plugin
@@ -46,13 +49,20 @@ impl Plugin for YruTremoloRs {
     fn run(&mut self, ports: &mut Ports, _features: &mut Self::AudioFeatures) {
         let depth = *ports.depth;
         let rate_smpl = *ports.rate / self.sr;
-        for (s_in, s_out) in Iterator::zip(ports.input.iter(), ports.output.iter_mut()) {
-            let gain = 1.0 - depth * 0.5*(f32::sin(2.0 * PI * self.progression) + 1.0);
+        let phase_rad = *ports.phase * PI / 180.0;
+        for (s_in, s_out) in Iterator::zip(
+            Iterator::zip(ports.l_in.iter(), ports.r_in.iter()),
+            Iterator::zip(ports.l_out.iter_mut(), ports.r_out.iter_mut()),
+        ) {
+            let omega = 2.0 * PI * self.progression;
+            let l_gain = 1.0 - depth * 0.5 * (f32::sin(omega) + 1.0);
+            let r_gain = 1.0 - depth * 0.5 * (f32::sin(omega+phase_rad) + 1.0);
             self.progression += rate_smpl;
             if self.progression > 1.0 {
                 self.progression -= 1.0;
             }
-            *s_out = gain * s_in;
+            *s_out.0 = l_gain * s_in.0;
+            *s_out.1 = r_gain * s_in.1;
         }
     }
 }
